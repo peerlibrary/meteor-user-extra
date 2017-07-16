f07c7b8b083b573edaf986ca7b7117518b052245
Mitar <mitar.git@tnode.com>
2017-07-15 23:04:36 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peerlibrary:user-extra',
   summary: "Extended Meteor.userId() and Meteor.user()",
-  version: '0.2.0',
+  version: '0.3.0',
   git: 'https://github.com/peerlibrary/meteor-user-extra.git'
 });
 
